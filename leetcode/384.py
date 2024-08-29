c00e81689013cd697b319249ae5760dd0e9fb8e4
Fiona Verzivolli <139073560+FionaVerzivolli@users.noreply.github.com>
2024-08-29 19:31:16 -0400

    Create 384.py

---
+++ leetcode/384.py
@@ -0,0 +1,15 @@
+import random
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = nums[:]  
+        self.array = nums[:]     
+        
+    def reset(self) -> List[int]:
+        self.array = self.original[:]  
+        return self.array
+
+    def shuffle(self) -> List[int]:
+        shuffled = self.array[:]  
+        random.shuffle(shuffled)  
+        return shuffled
