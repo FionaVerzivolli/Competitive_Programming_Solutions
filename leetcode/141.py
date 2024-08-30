778c9674e17c2974e76a8e21bd375fd9cf1bb80a
Fiona Verzivolli <139073560+FionaVerzivolli@users.noreply.github.com>
2024-08-30 18:00:08 -0400

    Update 141.py

--- leetcode/141.py
+++ leetcode/141.py
@@ -1,17 +1,12 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        nodes = {}
+        visited = []
         curr = head
-        while curr is not None:
-            if curr in nodes:
-                return True
-            nodes[curr] = 1
-            curr = curr.next
-        return False
         
+        while curr:
+            if curr in visited:
+                return True
+            visited.append(curr)
+            curr = curr.next
+        
+        return False
