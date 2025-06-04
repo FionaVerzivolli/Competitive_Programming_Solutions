5ffd9746e87b6027a30fb8d624115a015deeae2e
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2025-06-03 23:15:48 -0400

    start new question

---
+++ usaco/bronze/basic_complete_search/sleepinginclass.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+// difficulty: tagged as "hard"
+
+void solve() {
+    int ret = 0;
+    cout << ret;
+    
+}
+
+int main(){
+    //Speed up
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int testcases = 1;
+
+    //cin >> testcases; 
+
+    for (int number_of_total_test_cases = 1; number_of_total_test_cases <= testcases; ++number_of_total_test_cases){
+
+        //Google
+        //cout<<"Case #"<<number_of_total_test_cases<<": ";
+
+        // Normal
+        solve();
+
+    }
+
+    //cout << elapsedTime << '\n';
+}
